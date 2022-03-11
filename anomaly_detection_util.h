74900f39ab3e3be986aaf0479bf6cb9d45092019
Yossi Maatook <79411610+yossix07@users.noreply.github.com>
2022-03-11 22:30:07 +0200

    Create anomaly_detection_util.h

---
+++ anomaly_detection_util.h
@@ -0,0 +1,96 @@
+/*
+ * anomaly_detection_util.h
+ *
+ * Author: Yossi Maatook
+ * Author: Osher Elhadad
+ */
+
+#ifndef ANOMALYDETECTORUTIL_H_
+#define ANOMALYDETECTORUTIL_H_
+
+#include <memory>
+#include <vector>
+#include <cmath>
+#define DOUBLE_POW 2
+#define DEFAULT_VALUE 0
+#define MIDDLE_DEST 2
+
+
+// returns the average of the array's members
+float avg(const float* x, int size) noexcept(false);
+
+// returns the variance of X
+float var(float* x, int size) noexcept(false);
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size) noexcept(false);
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size);
+
+// line has a - slope and b - constant
+class Line {
+public:
+	float a,b;
+
+    // default constructor
+	Line(): a(DEFAULT_VALUE), b(DEFAULT_VALUE) {}
+
+    // constructor
+    Line(float a, float b): a(a), b(b) {}
+
+    // evaluate the y value at the given x
+	float f(float x) const {
+		return a * x + b;
+	}
+};
+
+// point has x and y
+class Point {
+public:
+	float x, y;
+
+    // constructor
+	Point(float x, float y): x(x), y(y) {}
+
+    // getter for point's x
+    float getX() const {
+        return this->x;
+    }
+
+    // setter for point's x
+    void setX(float newX) {
+        this->x = newX;
+    }
+
+    // getter for point's y
+    float getY() const {
+        return this->y;
+    }
+
+    // setter for point's y
+    void setY(float newY) {
+        this->y = newY;
+    }
+
+    // returns euclidean distance between current point and the received point p
+    float distance(const Point& p) const {
+        return sqrtf(powf(this->x - p.getX(), DOUBLE_POW) + powf(this->y - p.getY(), DOUBLE_POW));
+    }
+
+    // returns the middle points between current points and the received point
+    Point middlePoint(const Point& p) const {
+        return {(this->x + p.getX()) / MIDDLE_DEST , (this->y + p.getY()) / MIDDLE_DEST};
+    }
+};
+
+// performs a linear regression and returns the line equation
+Line linear_reg(Point** points, int size) noexcept(false);
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p, Point** points, int size) noexcept(false);
+
+// returns the deviation between point p and the line
+float dev(Point p, Line l);
+
+#endif
