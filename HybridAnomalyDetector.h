9f51079aa5caf3157cee7fff456f56196ca276c2
Yossi Maatook <79411610+yossix07@users.noreply.github.com>
2022-03-11 22:26:19 +0200

    Create HybridAnomalyDetector.h

---
+++ HybridAnomalyDetector.h
@@ -0,0 +1,33 @@
+/*
+ * HybridAnomalyDetector.h
+ *
+ * Author: Yossi Maatook
+ * Author: Osher Elhadad
+ */
+
+#ifndef HYBRIDANOMALYDETECTOR_H_
+#define HYBRIDANOMALYDETECTOR_H_
+
+#include "SimpleAnomalyDetector.h"
+#include "minCircle.h"
+
+class HybridAnomalyDetector:public SimpleAnomalyDetector {
+private:
+    float minCorrelation;
+
+public:
+
+    // constructor - get a minCorrelation (or 0.5 by default)
+	HybridAnomalyDetector(): minCorrelation(0.5) {};
+    explicit HybridAnomalyDetector(float _minCorrelation): minCorrelation(_minCorrelation) {};
+
+    // return if there is an anomaly or not
+    virtual bool isAnomaly(const correlatedFeatures* c, Point* &point);
+
+    // add to cf if there is a correlation or don't add
+    virtual void addIfCorrelate(float maxPearson, Point** points, size_t size, string f1, string f2);
+
+
+};
+
+#endif /* HYBRIDANOMALYDETECTOR_H_ */
